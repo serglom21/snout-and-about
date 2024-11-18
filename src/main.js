0ba8754a73350319ab6746106019475a655deba4
sergiosentry <109162568+serglom21@users.noreply.github.com>
2024-11-18 15:19:38 -0500

    add domain to tracePropagationTargets

--- src/main.js
+++ src/main.js
@@ -11,17 +11,18 @@ Sentry.init({
     dsn: process.env.VUE_APP_SENTRY_DSN,
     release: process.env.VUE_APP_RELEASE,
     trackComponents: true,
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
     integrations: [
         Sentry.browserTracingIntegration({router}),
         Sentry.replayIntegration(),
         Sentry.browserProfilingIntegration()
       ],
     tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 1.0,
-    replaysOnErrorSampleRate: 1.0,
     profilesSampleRate: 1.0,
     tracePropagationTargets: [
-      "127.0.0.1:8000"
+      "127.0.0.1:8000",
+      "herokuapp.com"
     ]
 })
 
